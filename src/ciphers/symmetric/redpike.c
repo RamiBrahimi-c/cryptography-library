0eccc836e95160f487a91bba1cf851da509bd400
RamiBrahimi-c <ramibrahimi02@gmail.com>
2026-07-28 12:03:51 +0100

    feat && fix : redpike works with the block mode api AND FIXED DECRYPTION WHO WAS NOT WORKING BEFORE

--- src/ciphers/symmetric/redpike.c
+++ src/ciphers/symmetric/redpike.c
@@ -9,12 +9,16 @@
 #define ROTR(X, R) (((X) >> ((R) & 31)) | ((X) << (32 - ((R) & 31))))
 
 typedef uint32_t word;
-
+#define REDPIKE_BLOCK_SIZE 8
 
 // all https://web.archive.org/web/20150602092816/http://permalink.gmane.org/gmane.comp.security.cypherpunks/3680
 
-static void redpike_encrypt_block(uchar_t * x1, const uchar_t * k1)
+static void redpike_encrypt_block(uchar_t * x1 , uchar_t *output, const void * key)
 {
+    assert(key != NULL && "key is null");
+    RedpikeKey *redpike_key = (RedpikeKey *) (key) ;
+	assert(redpike_key != NULL && "key is null");
+	uchar_t *k1 = redpike_key->key ;
   word x[2] ; 
   word k[2] ; 
 
@@ -53,23 +57,28 @@ static void redpike_encrypt_block(uchar_t * x1, const uchar_t * k1)
   rk0 = x[0]; x[0] = x[1]; x[1] = rk0;
 
 
-  x1[0] = x[0] >> 24 ; 
-  x1[1] = x[0] >> 16 ; 
-  x1[2] = x[0] >> 8 ; 
-  x1[3] = x[0]  ; 
+  output[0] = x[0] >> 24 ; 
+  output[1] = x[0] >> 16 ; 
+  output[2] = x[0] >> 8 ; 
+  output[3] = x[0]  ; 
 
 
-  x1[4] = x[1] >> 24 ; 
-  x1[5] = x[1] >> 16 ; 
-  x1[6] = x[1] >> 8 ; 
-  x1[7] = x[1]  ; 
+  output[4] = x[1] >> 24 ; 
+  output[5] = x[1] >> 16 ; 
+  output[6] = x[1] >> 8 ; 
+  output[7] = x[1]  ; 
 
 
 
 }
 
-static void redpike_decrypt_block(uchar_t * x1, const uchar_t * k1)
+static void redpike_decrypt_block(uchar_t * x1 , uchar_t *output, const void * key)
 {
+    assert(key != NULL && "key is null");
+    RedpikeKey *redpike_key = (RedpikeKey *) (key) ;
+    assert(redpike_key != NULL && "key is null");
+	uchar_t *k1 = redpike_key->key ;
+	
   word x[2] ; 
   word k[2] ; 
   x[0] = (x1[0] << 24) | (x1[1] << 16) | (x1[2] << 8) | (x1[3]) ; 
@@ -85,9 +94,23 @@ static void redpike_decrypt_block(uchar_t * x1, const uchar_t * k1)
     k[1] - CONST * (ROUNDS + 1),
     k[0] + CONST * (ROUNDS + 1)
   };
+  void *new_key = malloc(sizeof(RedpikeKey));
+  RedpikeKey *new_key_handle = (RedpikeKey*) new_key ;
+  memcpy(new_key_handle , redpike_key , sizeof(RedpikeKey)) ; 
 
-  redpike_encrypt_block(x, dk);
+  new_key_handle->key[0] = dk[0] >> 24 ;
+  new_key_handle->key[1] = dk[0] >> 16;
+  new_key_handle->key[2] = dk[0] >> 8;
+  new_key_handle->key[3] = dk[0] & 0xff  ;
 
+  new_key_handle->key[4] = dk[1] >> 24 ;
+  new_key_handle->key[5] = dk[1] >> 16;
+  new_key_handle->key[6] = dk[1] >> 8;
+  new_key_handle->key[7] = dk[1] & 0xff ;
+
+
+  redpike_encrypt_block(x1,output ,new_key);
+  free(new_key) ; 
 
 }
 
@@ -95,32 +118,18 @@ static void redpike_decrypt_block(uchar_t * x1, const uchar_t * k1)
 
 
 void redpike_encrypt(const uchar_t* input, uchar_t* output , int length , const void* key) {
-    assert(key != NULL && "key is null");
-    RedpikeKey *redpike_key = (RedpikeKey *) (key) ;
 
+	uchar_t *iv = malloc(sizeof(uchar_t)*REDPIKE_BLOCK_SIZE) ;
+    blockcipher_encrypt_modeop(input , output , iv , length , REDPIKE_BLOCK_SIZE , key , redpike_encrypt_block) ;
+    free(iv);    
     
-
-	for (size_t i = 0; i < length /8; i++)
-	{
-		redpike_encrypt_block(input + i *8 , redpike_key->key) ; 
-	}
-    
-    // maybe we need to handle this better next time ...
-    memcpy(output , input , sizeof(uchar_t)*length) ; 
 }
 
 void redpike_decrypt(const uchar_t* input, uchar_t* output , int length , const void* key) {
-    assert(key != NULL && "key is null");
-    RedpikeKey *redpike_key = (RedpikeKey *) (key) ;
-    
-    
-    
-	for (size_t i = 0; i < length /8; i++)
-	{
-        redpike_decrypt_block(input + i *8 , redpike_key->key) ; 
-	}
-    memcpy(output , input , sizeof(uchar_t)*length) ; 
-	
+
+	uchar_t *iv = malloc(sizeof(uchar_t)*REDPIKE_BLOCK_SIZE) ;
+    blockcipher_decrypt_modeop(input , output , iv , length , REDPIKE_BLOCK_SIZE , key , redpike_decrypt_block) ;
+    free(iv);    	
     
 }
 
