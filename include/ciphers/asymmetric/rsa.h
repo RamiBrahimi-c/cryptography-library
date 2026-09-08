30109adcd237e78ac23d824e8434cf4bb49fd753
RamiBrahimi-c <ramibrahimi02@gmail.com>
2026-09-08 16:07:42 +0100

    am i hearing you right ? got rid of gmp and put bigra9m there ????????????????????

--- include/ciphers/asymmetric/rsa.h
+++ include/ciphers/asymmetric/rsa.h
@@ -5,12 +5,13 @@
 #include "../../cipher_interface.h"
 #include "../../common/constants.h"
 #include <stdint.h>
-#include <gmp.h>   // GMP header
+// #include "../../third-party/big-ar9am/include/bigra9m.h"
+#include "../../../third-party/big-ar9am/include/bigra9m.h"
 
 typedef struct {
-    mpz_t n;      // modulus
-    mpz_t e;      // public exponent
-    mpz_t d;      // private exponent (only for private key)
+    BigRa9m n;      // modulus
+    BigRa9m e;      // public exponent
+    BigRa9m d;      // private exponent (only for private key)
     int   bits;   // key size (512, 1024, 2048)
     int   is_private;   // 1 if we have d, 0 if only public
 } RsaKey;
@@ -18,7 +19,7 @@ typedef struct {
 
 void rsa_encrypt(const uchar_t* input, uchar_t* output , int length , const void* key);
 void rsa_decrypt(const uchar_t* input, uchar_t* output , int length , const void* key);
-void rsa_set_key(void* key_struct, const char* key_str);
+void rsa_set_key(void* key_struct, const char* key_str , size_t key_len);
 void rsa_free_key(void* key_struct);
 
 Cipher* get_des_cipher(void);
@@ -26,7 +27,6 @@ Cipher* get_des_cipher(void);
 void test_rsa_32bytes(int bits) ;
 
 
-#include <gmp.h>
 #include <stdlib.h>
 #include <time.h>
 
@@ -34,19 +34,10 @@ void test_rsa_32bytes(int bits) ;
 
 void rsa_generate_keypair(RsaKey* key, int bits, unsigned long e_value);
 
-
-
-
-
-
-
-
-
 void rsa_export_key(const RsaKey* key);
 
 void sayhi() ;
 
-
-
+void hybrid_test(void) ; 
 
 #endif
--- src/ciphers/asymmetric/rsa.c
+++ src/ciphers/asymmetric/rsa.c
@@ -1,61 +1,92 @@
-#include <gmp.h>
 #include <stdlib.h>
 #include <time.h>
-
+// #include "bigra9m.h"
 
 #include "../../../include/ciphers/asymmetric/rsa.h"
 #include "../../../include/ciphers/symmetric/aes.h"
 #include "../../../include/common/utils.h"
 #include "../../../include/common/constants.h"
 
+#define DEBUG_GENKEYPAIR 0
+#define DEBUG_RSA_ENC 0
+#define DEBUG_RSA_DEC 0
+
 void rsa_generate_keypair(RsaKey* key, int bits, unsigned long e_value)
 {
-    gmp_randstate_t state;
-    gmp_randinit_mt(state);
-    unsigned long seed = time(NULL);
-    if (seed == 0) seed = 42;
-    gmp_randseed_ui(state, seed);
-
-    mpz_t p, q, n, phi, p1, q1, gcd;
-    mpz_inits(p, q, n, phi, p1, q1, gcd, NULL);
-
-    int prime_bits = bits / 2;
-
+    BigRa9m p, q, n, phi, p1, q1, gcd , big_1;
+    bigra9m_inits(&p, &q, &n, &phi, &p1, &q1, &gcd , &big_1, NULL);
+    bigra9m_assign_uint64_t(&big_1 , 1);
+    
+    int prime_bits = bits / 2 ;
+    
     // Generate p
-    mpz_urandomb(p, state, prime_bits);
-    mpz_setbit(p, prime_bits - 1);  // ensure exactly prime_bits
-    mpz_nextprime(p, p);
 
-    // Generate q (different from p)
-    do {
-        mpz_urandomb(q, state, prime_bits);
-        mpz_setbit(q, prime_bits - 1);
-        mpz_nextprime(q, q);
-    } while (mpz_cmp(p, q) == 0);
+    generate_prime_bigra9m3(&p , prime_bits + 1) ; 
+
+    #if  DEBUG_GENKEYPAIR
+        printf("bits : %d \n" , bits) ;
+        printf("prime bits : %d \n" , prime_bits) ;
+        printf("p : ") ;
+        bigra9m_print(p) ; 
+    #endif 
+
+    do
+    {
+        generate_prime_bigra9m3(&q , prime_bits) ; 
+        
+    } while (bigra9m_isEqualNum(p , q));
+
+    #if DEBUG_GENKEYPAIR
+        printf("q : ") ;
+        bigra9m_print(q) ; 
+    #endif
 
     // n = p * q
-    mpz_mul(key->n, p, q);
-
+    bigra9m_mul( &p, &q , &key->n);
+    
     // phi = (p-1)*(q-1)
-    mpz_sub_ui(p1, p, 1);
-    mpz_sub_ui(q1, q, 1);
-    mpz_mul(phi, p1, q1);
-
+    bigra9m_sub(&p , &big_1 , &p1 ) ; 
+    bigra9m_sub(&q , &big_1 , &q1 ) ; 
+    bigra9m_mul(&p1, &q1 , &phi);
+    
+    #if DEBUG_GENKEYPAIR
+        printf("phi : ") ; 
+        bigra9m_print(phi) ; 
+    #endif
+    
     // e
-    mpz_set_ui(key->e, e_value);
-    mpz_gcd(gcd, key->e, phi);
-    if (mpz_cmp_ui(gcd, 1) != 0) {
-        mpz_set_ui(key->e, 65537);
+    bigra9m_assign_uint64_t(&key->e , e_value) ; 
+    #if DEBUG_GENKEYPAIR
+        printf("e : \n") ; 
+    
+        bigra9m_print(key->e) ;
+    #endif
+    
+    bigra9m_gcd(&key->e ,&phi , &gcd) ;
+    
+    #if DEBUG_GENKEYPAIR
+        printf("gcd : \n") ; 
+        bigra9m_print(gcd) ;
+    #endif
+    
+    if (!bigra9m_isEqual_uint64(gcd ,1 ))
+    {
+        bigra9m_assign_uint64_t(&key->e , 65537) ; 
+        
     }
-
+    
     // d = e⁻¹ mod phi
-    mpz_invert(key->d, key->e, phi);
+    bigra9m_modinverse(&key->e , &phi , &key->d) ; 
+    
+    #if DEBUG_GENKEYPAIR
+        printf("d : ") ; 
+        bigra9m_print(key->d) ;
+    #endif
 
     key->bits = bits;
     key->is_private = 1;
 
-    mpz_clears(p, q, n, phi, p1, q1, gcd, NULL);
-    gmp_randclear(state);
+    bigra9m_clears(&p, &q, &n, &phi, &p1, &q1, &gcd ,&big_1 , NULL);
 }
 
 
@@ -65,59 +96,80 @@ void rsa_encrypt(const uchar_t* input, uchar_t* output, int length, const void* 
     size_t out_bytes = (rsa->bits + 7) / 8;
     memset(output, 0, out_bytes);
 
-    mpz_t m, c;
-    mpz_inits(m, c, NULL);
-    mpz_import(m, length, 1, 1, 0, 0, input);
-    mpz_powm(c, m, rsa->e, rsa->n);
+    BigInt m , c ;
+    bigra9m_inits(&m , &c , NULL) ; 
+    bigra9m_import(&m , length , sizeof(input[0]) , input ) ; 
+    #if DEBUG_RSA_ENC
+        printf("m : \n") ; 
+        bigra9m_print(m) ; 
+    #endif
+    
+    bigra9m_modular_exponentiation(&m , &rsa->e , &rsa->n , &c ) ; 
+    
+    #if DEBUG_RSA_ENC
+        printf("c : \n") ; 
+        bigra9m_print(c) ; 
+    #endif
 
     // Export to temporary buffer, then right-align into output
     size_t count = 0;
     unsigned char temp[1024];
-    mpz_export(temp, &count, 1, 1, 0, 0, c);
+    bigra9m_export(temp , &count , sizeof(temp[0]) , &c) ; 
 
+    #if DEBUG_RSA_ENC
+        printf("out bytes : %ld \n" , out_bytes) ; 
+        printf("count : %ld \n" , count) ;
+    #endif
+ 
     if (count <= out_bytes) {
         memcpy(output + (out_bytes - count), temp, count);
     } else {
         memcpy(output, temp + (count - out_bytes), out_bytes);
     }
 
-    mpz_clears(m, c, NULL);
+    bigra9m_clears(&m , &c , NULL) ; 
 }
 
 void rsa_decrypt(const uchar_t* input, uchar_t* output, int length, const void* key)
 {
     const RsaKey* rsa = (const RsaKey*)key;
     if (!rsa || !rsa->is_private || length <= 0) return;
-
     size_t out_bytes = (rsa->bits + 7) / 8;
-
-    mpz_t c, m;
-    mpz_inits(c, m, NULL);
-    mpz_import(c, length, 1, 1, 0, 0, input);
-    mpz_powm(m, c, rsa->d, rsa->n);
+    // printf("buf boooo \n") ; 
+    // PRINT_ARRAY_NAI(input , length , "%x") ; 
+    BigInt m , c ;
+    bigra9m_inits(&m , &c , NULL) ; 
+    bigra9m_import(&c , length , sizeof(input[0]) , input ) ; 
+    #if DEBUG_RSA_DEC
+    printf("c : \n") ; 
+    bigra9m_print(c) ; 
+    #endif
+    bigra9m_modular_exponentiation(&c , &rsa->d , &rsa->n , &m ) ; 
+    
+    #if DEBUG_RSA_DEC
+    printf("m : \n") ; 
+    bigra9m_print(m) ; 
+    #endif
 
     // Export to temp, then copy right‑aligned into output buffer
-    unsigned char temp[1024];   // up to 2048 bits = 256 bytes, safe
     size_t count = 0;
-    mpz_export(temp, &count, 1, 1, 0, 0, m);
-
-    // Zero the whole output buffer
-    memset(output, 0, out_bytes);
-
-    // Right‑align the actual bytes
+    unsigned char temp[1024];
+    bigra9m_export(temp , &count , sizeof(temp[0]) , &m) ; 
+    
     if (count <= out_bytes) {
         memcpy(output + (out_bytes - count), temp, count);
     } else {
-        // Should not happen, but just copy the last out_bytes bytes
         memcpy(output, temp + (count - out_bytes), out_bytes);
     }
 
-    mpz_clears(c, m, NULL);
+    #if DEBUG_RSA_DEC
+        printf("fuck you\n") ; 
+    #endif
+    bigra9m_clears(&m , &c , NULL) ;
 }
 
 
-
-void rsa_set_key(void* key_struct, const char* key_str)
+void rsa_set_key(void* key_struct, const char* key_str , size_t key_len)
 {
     RsaKey* rsa = (RsaKey*)key_struct;
     // parse "n_hex:e_hex:d_hex:bits"
@@ -127,15 +179,18 @@ void rsa_set_key(void* key_struct, const char* key_str)
     char* tok_d = strtok(NULL, ":");
     char* tok_bits = strtok(NULL, ":");
     if (tok_n && tok_e && tok_d && tok_bits) {
-        mpz_set_str(rsa->n, tok_n, 16);
-        mpz_set_str(rsa->e, tok_e, 16);
-        mpz_set_str(rsa->d, tok_d, 16);
+        bigra9m_assign_str(&rsa->n, tok_n);
+        bigra9m_assign_str(&rsa->e, tok_e);
+        bigra9m_assign_str(&rsa->d, tok_d);
+        // mpz_set_str(rsa->n, tok_n, 16);
+        // mpz_set_str(rsa->e, tok_e, 16);
+        // mpz_set_str(rsa->d, tok_d, 16);
         rsa->bits = atoi(tok_bits);
         rsa->is_private = 1;
         printf("wth1\n") ; 
     } else if (tok_n && tok_e && tok_bits) {
-        mpz_set_str(rsa->n, tok_n, 16);
-        mpz_set_str(rsa->e, tok_e, 16);
+        bigra9m_assign_str(&rsa->n, tok_n);
+        bigra9m_assign_str(&rsa->e, tok_e);
         rsa->bits = atoi(tok_bits);
         rsa->is_private = 0;
         printf("wth2\n") ; 
@@ -150,21 +205,22 @@ void rsa_set_key(void* key_struct, const char* key_str)
 void rsa_free_key(void* key_struct)
 {
     RsaKey* rsa = (RsaKey*)key_struct;
-    mpz_clears(rsa->n, rsa->e, rsa->d, NULL);
+    bigra9m_clears(&rsa->n, &rsa->e, &rsa->d, NULL);
+    // mpz_clears(rsa->n, rsa->e, rsa->d, NULL);
 }
 
 
 
 void rsa_export_key(const RsaKey* key) {
     printf("Modulus n (hex): ");
-    mpz_out_str(stdout, 16, key->n);
-    printf("\nPublic exponent e (hex): ");
-    mpz_out_str(stdout, 16, key->e);
+    bigra9m_print(key->n) ; 
+    printf("Public exponent e (hex): ");
+    bigra9m_print(key->e) ; 
     if (key->is_private) {
-        printf("\nPrivate exponent d (hex): ");
-        mpz_out_str(stdout, 16, key->d);
+        printf("Private exponent d (hex): ");
+        bigra9m_print(key->d) ; 
     }
-    printf("\nBits: %d\n", key->bits);
+    printf("Bits: %d\n", key->bits);
 }
 
 void test_rsa_32bytes(int bits) {
@@ -172,7 +228,8 @@ void test_rsa_32bytes(int bits) {
     fflush(stdout);
 
     RsaKey key;
-    mpz_inits(key.n, key.e, key.d, NULL);
+    // // // // // // // mpz_inits(key., key.e, key.d, NULL);
+    bigra9m_inits(&key.e , &key.n , &key.d , NULL) ; 
 
     rsa_generate_keypair(&key, bits, 65537);
     printf("generated key pairs successfully for %d bits\n", bits);
@@ -199,30 +256,30 @@ void test_rsa_32bytes(int bits) {
     free(decrypted);
     rsa_free_key(&key);
 }
+
+
 void sayhi() {
     printf("hi fuck this all\n") ; 
 }
 
-// test_rsa_32bytes(512), test_rsa_32bytes(1024), test_rsa_32bytes(2048)
-
 
 void hybrid_test(void) {
+    printf("============================= hybrid test =============================\n") ; 
     printf("hi") ;
     RsaKey rsa;
-    mpz_inits(rsa.n, rsa.e, rsa.d, NULL);
+    bigra9m_inits(&rsa.n, &rsa.e, &rsa.d, NULL) ; 
+    // mpz_inits(rsa.n, rsa.e, rsa.d, NULL);
     rsa_generate_keypair(&rsa, 2048, 65537);
     
     // 1. Random AES‑256 key (32 bytes)
     uchar_t aes_key[32];
-    for (int i = 0; i < 32; i++) aes_key[i] = rand() & 0xFF;   // weak randomness
+    for (int i = 0; i < 32; i++) aes_key[i] = getRandomInt(0xcafe0) & 0xFF;   // weak randomness
     
     // 2. Encrypt AES key with RSA
     size_t rsa_out = (rsa.bits + 7) / 8;
     uchar_t encrypted_key[rsa_out];
     clock_t t1 = clock();
-    printf("hi\n") ;
     rsa_encrypt(aes_key,  encrypted_key , 32, &rsa);
-    printf("hi!!!!!!!!!!!\n") ;
     clock_t t2 = clock();
     double rsa_time = (double)(t2 - t1) / CLOCKS_PER_SEC;
 
@@ -248,4 +305,5 @@ void hybrid_test(void) {
     free(file_data);
     free(aes_cipher);
     rsa_free_key(&rsa);
+    printf("============================= end hybrid test =============================\n") ; 
 }
