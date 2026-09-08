5b28bf7a9b4048e087ff9c90695e87fc8a91d4cf
RamiBrahimi-c <ramibrahimi02@gmail.com>
2026-09-08 19:28:08 +0100

    fuck this

--- src/ciphers/asymmetric/dh.c
+++ src/ciphers/asymmetric/dh.c
@@ -8,7 +8,6 @@
 void dh_generate_params(BigRa9m* p, BigRa9m* g, int bits)
 {
     // Generate large prime
-    // generate_prime(p, bits);
     generate_prime_bigra9m3(p , bits) ; 
 
 
@@ -20,9 +19,6 @@ void dh_generate_params(BigRa9m* p, BigRa9m* g, int bits)
 
     bigra9m_sub(p , &big_1 , &p_minus_1) ; 
 
-    // mpz_t p_minus_1, q, test;
-    // mpz_inits(p_minus_1, q, test, NULL);
-    // mpz_sub_ui(p_minus_1, p, 1);
 
     unsigned long candidates[] = {2, 3, 5, 7, 11, 13};
     int found = 0;
@@ -30,21 +26,14 @@ void dh_generate_params(BigRa9m* p, BigRa9m* g, int bits)
     for (int i = 0; i < 6; i++) {
         bigra9m_assign_uint64_t(g , candidates[i] ) ; 
         bigra9m_div2(&p_minus_1 , &big_2 , &q ) ; 
-        // mpz_set_ui(g, candidates[i]);
-        // mpz_tdiv_q_ui(q, p_minus_1, 2);
 
         bigra9m_modular_exponentiation(g , &q , p , &test) ; 
 
-        // mpz_powm(test, g, q, p);
 
         if (!bigra9m_isEqual_uint64(test, 1)) {
             found = 1;
             break;
         }        
-        // if (mpz_cmp_ui(test, 1) != 0) {
-        //     found = 1;
-        //     break;
-        // }
     }
     if (!found) bigra9m_assign_uint64_t(g, 2);
 
@@ -54,26 +43,13 @@ void dh_generate_params(BigRa9m* p, BigRa9m* g, int bits)
 void dh_generate_keypair(DHParty* party, const BigRa9m* p, const BigRa9m* g)
 {
     bigra9m_assign(&party->p, *p);   // store p
-    // mpz_set(party->p, p);   // store p
-    
-    // gmp_randstate_t state;
-    // gmp_randinit_mt(state);
-    // gmp_randseed_ui(state, time(NULL) ^ rand());
 
     BigRa9m p_minus_1 , big_1; 
     bigra9m_inits(&p_minus_1 , &big_1 , NULL) ; 
     bigra9m_sub(p , &big_1 , &p_minus_1) ; 
-    // mpz_t p_minus_1;
-    // mpz_init(p_minus_1);
-    // mpz_sub_ui(p_minus_1, p, 1);
+
 
     setrandom_bigra9m2(&party->private_key , &p_minus_1) ; 
-    // mpz_urandomm(party->private_key, state, p_minus_1);
-    
-    // if (mpz_cmp_ui(party->private_key, 0) == 0)
-    //     mpz_set_ui(party->private_key, 1);
-
-    // mpz_powm(party->public_key, g, party->private_key, p);
     
     if (bigra9m_isEqual_uint64(party->private_key, 0) )
         bigra9m_assign_uint64_t(&party->private_key, 1);
@@ -82,8 +58,6 @@ void dh_generate_keypair(DHParty* party, const BigRa9m* p, const BigRa9m* g)
 
     bigra9m_clears(&p_minus_1 , &big_1 , NULL) ; 
 
-    // mpz_clear(p_minus_1);
-    // gmp_randclear(state);
 }
 
 
