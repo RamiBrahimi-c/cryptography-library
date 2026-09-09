5c6de7828f07509be3ef7ea07a7960b611d6c49c
RamiBrahimi-c <ramibrahimi02@gmail.com>
2026-09-09 19:04:27 +0100

    finally eh , no gmp here :)

--- include/ciphers/asymmetric/elgamal.h
+++ include/ciphers/asymmetric/elgamal.h
@@ -1,25 +1,29 @@
 #ifndef ELGAMAL_H
 #define ELGAMAL_H
 
-#include <gmp.h>
+#include "bigra9m.h"
 
 typedef struct {
-    mpz_t p;      // large prime (> 512 bits)
-    mpz_t g;      // generator
-    mpz_t x;      // private key (random)
-    mpz_t y;      // public key (y = g^x mod p)
+    BigRa9m p;      // large prime (> 512 bits)
+    BigRa9m g;      // generator
+    BigRa9m x;      // private key (random)
+    BigRa9m y;      // public key (y = g^x mod p)
     int bits;
 } ElGamalKey;
 
 typedef struct {
-    mpz_t c1;     // g^k mod p
-    mpz_t c2;     // M * y^k mod p
+    BigRa9m c1;     // g^k mod p
+    BigRa9m c2;     // M * y^k mod p
 } ElGamalCiphertext;
 
+
+
 void elgamal_generate_keypair(ElGamalKey* key, int bits);
-void elgamal_encrypt(ElGamalCiphertext* ct, const mpz_t message, const ElGamalKey* key);
-void elgamal_decrypt(mpz_t message, const ElGamalCiphertext* ct, const ElGamalKey* key);
+void elgamal_encrypt(ElGamalCiphertext* ct, const BigRa9m* message, const ElGamalKey* key);
+void elgamal_decrypt(BigRa9m* message, const ElGamalCiphertext* ct, const ElGamalKey* key);
 void elgamal_free_key(ElGamalKey* key);
 void elgamal_free_ciphertext(ElGamalCiphertext* ct);
+
+
 
 #endif
--- src/ciphers/asymmetric/elgamal.c
+++ src/ciphers/asymmetric/elgamal.c
@@ -5,115 +5,127 @@
 
 void elgamal_generate_keypair(ElGamalKey* key, int bits)
 {
-    gmp_randstate_t state;
-    gmp_randinit_mt(state);
-    unsigned long seed = time(NULL);
-    if (seed == 0) seed = 42;
-    gmp_randseed_ui(state, seed);
 
-    mpz_inits(key->p, key->g, key->x, key->y, NULL);
+    bigra9m_inits(&key->p, &key->g, &key->x, &key->y, NULL);
 
     // 1. Generate large prime p (> 512 bits)
-    mpz_urandomb(key->p, state, bits);
-    mpz_setbit(key->p, bits - 1);   // ensure exactly 'bits' bits
-    mpz_nextprime(key->p, key->p);
+    
+    generate_prime_bigra9m3(&key->p , bits) ; 
+
 
     // 2. Choose generator g (small prime like 2 or 5 works if safe prime, else random)
     // For simplicity, use g = 2 (check if it's a generator, else 3, 5...)
+
     unsigned long small_primes[] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29};
-    mpz_t p_minus_1, q, test;
-    mpz_inits(p_minus_1, q, test, NULL);
-    mpz_sub_ui(p_minus_1, key->p, 1);
     
+    BigRa9m p_minus_1, q, test , big_1 , big_2;
+    bigra9m_inits(&p_minus_1, &q, &test , &big_1 , &big_2, NULL);
+    bigra9m_assign_uint64_t(&big_1 , 1) ; 
+    bigra9m_assign_uint64_t(&big_2 , 2) ; 
+    bigra9m_sub(&key->p , &big_1 , &p_minus_1) ; 
+
     int found = 0;
     for (int i = 0; i < 10; i++) {
-        mpz_set_ui(key->g, small_primes[i]);
+
+        bigra9m_assign_uint64_t(&key->g ,small_primes[i] ) ; 
+
         // Quick check: g^((p-1)/2) mod p != 1 (for safe primes)
-        mpz_tdiv_q_ui(q, p_minus_1, 2);
-        mpz_powm(test, key->g, q, key->p);
-        if (mpz_cmp_ui(test, 1) != 0) {
+        
+        bigra9m_div2(&p_minus_1 , &big_2 , &q) ;
+        bigra9m_modular_exponentiation(&key->g  , &q , &key->p , &test ) ; 
+        if (!bigra9m_isEqual_uint64(test, 1) ) {
             found = 1;
             break;
         }
+        
     }
     if (!found) {
         // Fallback: just use 2
-        mpz_set_ui(key->g, 2);
+        bigra9m_assign_uint64_t(&key->g, 2);
+        
     }
 
     // 3. Private key x: random in [1, p-2]
-    mpz_urandomm(key->x, state, p_minus_1);
-    if (mpz_cmp_ui(key->x, 0) == 0) mpz_set_ui(key->x, 1);
+    setrandom_bigra9m2(&key->x , &p_minus_1) ; 
+    if (bigra9m_is_zero(key->x)) // my rand is weird and perhaps it might just give a 0
+    {
+        bigra9m_assign_uint64_t(&key->x , 1) ; // not sure about ts ngl
+    }
+    
+
 
     // 4. Public key y = g^x mod p
-    mpz_powm(key->y, key->g, key->x, key->p);
+    
+    bigra9m_modular_exponentiation(&key->g, &key->x, &key->p , &key->y) ; 
 
     key->bits = bits;
 
-    mpz_clears(p_minus_1, q, test, NULL);
-    gmp_randclear(state);
+    bigra9m_clears(&p_minus_1, &q, &test , &big_1 , &big_2, NULL);
+    
 }
 
-void elgamal_encrypt(ElGamalCiphertext* ct, const mpz_t message, const ElGamalKey* key)
+
+void elgamal_encrypt(ElGamalCiphertext* ct, const BigRa9m* message, const ElGamalKey* key)
 {
-    gmp_randstate_t state;
-    gmp_randinit_mt(state);
-    
-    // Use time + a static counter to guarantee different seeds
-    static unsigned long counter = 0;
-    unsigned long seed = time(NULL) ^ (counter++ << 16);
-    gmp_randseed_ui(state, seed);
 
-    mpz_inits(ct->c1, ct->c2, NULL);
+    bigra9m_inits(&ct->c1, &ct->c2, NULL);
 
-    mpz_t k, p_minus_1;
-    mpz_inits(k, p_minus_1, NULL);
-    mpz_sub_ui(p_minus_1, key->p, 1);
-    mpz_urandomm(k, state, p_minus_1);
-    if (mpz_cmp_ui(k, 0) == 0) mpz_set_ui(k, 1);
+    BigRa9m k, p_minus_1 , one_big;
+    bigra9m_inits(&k, &p_minus_1 , &one_big, NULL);
+    bigra9m_assign_uint64_t(&one_big , 1) ;
+
+    bigra9m_sub(&key->p , &one_big , &p_minus_1) ;
+    setrandom_bigra9m2(&k , &p_minus_1) ; 
+    if (bigra9m_isEqual_uint64(k , 0)) bigra9m_assign_uint64_t(&k , 1) ; 
+
 
     // c1 = g^k mod p
-    mpz_powm(ct->c1, key->g, k, key->p);
-
+    bigra9m_modular_exponentiation( &key->g, &k, &key->p , &ct->c1 );
+    // mpz_powm(ct->c1, key->g, k, key->p);
+    
     // c2 = M * y^k mod p
-    mpz_t yk;
-    mpz_init(yk);
-    mpz_powm(yk, key->y, k, key->p);
-    mpz_mul(ct->c2, message, yk);
-    mpz_mod(ct->c2, ct->c2, key->p);
+    BigRa9m yk;
+    bigra9m_init(&yk);
+    bigra9m_modular_exponentiation( &key->y, &k, &key->p , &yk );
+    bigra9m_mul(message , &yk , &ct->c2) ; 
+    bigra9m_mod( ct->c2,  key->p , &ct->c2 );
 
-    mpz_clears(k, p_minus_1, yk, NULL);
-    gmp_randclear(state);
+
+    bigra9m_clears(&k, &p_minus_1 , &one_big, NULL);
 }
 
 
-
-
-void elgamal_decrypt(mpz_t message, const ElGamalCiphertext* ct, const ElGamalKey* key)
+void elgamal_decrypt(BigRa9m* message, const ElGamalCiphertext* ct, const ElGamalKey* key)
 {
     // M = c2 * (c1^x)^(-1) mod p
-    mpz_t s, s_inv;
-    mpz_inits(s, s_inv, NULL);
+    BigRa9m s, s_inv;
+    bigra9m_inits(&s, &s_inv, NULL);
 
     // s = c1^x mod p
-    mpz_powm(s, ct->c1, key->x, key->p);
+    bigra9m_modular_exponentiation( &ct->c1, &key->x, &key->p , &s);
 
     // s_inv = s^(-1) mod p
-    mpz_invert(s_inv, s, key->p);
+    bigra9m_modinverse(&s , &key->p , &s_inv ) ; 
 
+    
     // M = c2 * s_inv mod p
-    mpz_mul(message, ct->c2, s_inv);
-    mpz_mod(message, message, key->p);
 
-    mpz_clears(s, s_inv, NULL);
+    bigra9m_mul( &ct->c2, &s_inv , message);
+    bigra9m_mod( *message, key->p , message);
+
+
+    bigra9m_clears(&s, &s_inv, NULL);
 }
+
+
 
 void elgamal_free_key(ElGamalKey* key)
 {
-    mpz_clears(key->p, key->g, key->x, key->y, NULL);
+    bigra9m_clears(&key->p, &key->g, &key->x, &key->y, NULL);
 }
 
 void elgamal_free_ciphertext(ElGamalCiphertext* ct)
 {
-    mpz_clears(ct->c1, ct->c2, NULL);
+    bigra9m_clears(&ct->c1, &ct->c2, NULL);
 }
+
