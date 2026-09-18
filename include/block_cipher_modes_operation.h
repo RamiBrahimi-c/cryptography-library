5f6cddb3c834ade32144d8b83fe250628b43f8f7
RamiBrahimi-c <ramibrahimi02@gmail.com>
2026-09-18 15:24:21 +0100

    major changes such as the way of handling error (stderr+return int)

--- include/block_cipher_modes_operation.h
+++ include/block_cipher_modes_operation.h
@@ -11,27 +11,38 @@
 void blockcipher_encrypt_modeop(uchar_t *input , uchar_t *output ,uchar_t *iv , size_t length , size_t block_size  ,const void* key ,void (*encrypt_block)(const uchar_t* , uchar_t*, const void* )) ;
 void blockcipher_decrypt_modeop(uchar_t *input , uchar_t *output ,uchar_t *iv , size_t length , size_t block_size  ,const void* key ,void (*decrypt_block)(const uchar_t* , uchar_t*, const void* )) ;
 
+// returns 0 on success otherwise non-zero for error and it woll be reported to stderr
+int ecb_encrypt(uchar_t *input , uchar_t *output , size_t length , size_t block_size  ,const void* key ,int (*encrypt_block)(const uchar_t* , uchar_t* , const void* ) ) ;
+// returns 0 on success otherwise non-zero for error and it woll be reported to stderr
+int ecb_decrypt(uchar_t *input , uchar_t *output , size_t length , size_t block_size  ,const void* key ,int (*decrypt_block)(const uchar_t* , uchar_t* , const void* ) ) ;
 
-void ecb_encrypt(uchar_t *input , uchar_t *output , size_t length , size_t block_size  ,const void* key ,void (*encrypt_block)(const uchar_t* , uchar_t* , const void* ) ) ;
-void ecb_decrypt(uchar_t *input , uchar_t *output , size_t length , size_t block_size  ,const void* key ,void (*decrypt_block)(const uchar_t* , uchar_t* , const void* ) ) ;
+// returns 0 on success otherwise non-zero for error and it woll be reported to stderr
+int cbc_encrypt(uchar_t *input , uchar_t *output , uchar_t *iv , size_t length , size_t block_size  ,const void* key ,int (*encrypt_block)(const uchar_t* , uchar_t*, const void* ) ) ;
+// returns 0 on success otherwise non-zero for error and it woll be reported to stderr
+int cbc_decrypt(uchar_t *input , uchar_t *output , uchar_t *iv , size_t length , size_t block_size  ,const void* key ,int (*decrypt_block)(const uchar_t* , uchar_t*, const void* ) ) ;
 
-void cbc_encrypt(uchar_t *input , uchar_t *output , uchar_t *iv , size_t length , size_t block_size  ,const void* key ,void (*encrypt_block)(const uchar_t* , uchar_t*, const void* ) ) ;
-void cbc_decrypt(uchar_t *input , uchar_t *output , uchar_t *iv , size_t length , size_t block_size  ,const void* key ,void (*decrypt_block)(const uchar_t* , uchar_t*, const void* ) ) ;
-
-void ofb_encrypt(uchar_t *input , uchar_t *output , uchar_t *iv , size_t length , size_t block_size  ,const void* key ,void (*encrypt_block)(const uchar_t* , uchar_t*, const void* ) ) ;
-void ofb_decrypt(uchar_t *input , uchar_t *output , uchar_t *iv , size_t length , size_t block_size  ,const void* key ,void (*decrypt_block)(const uchar_t* , uchar_t*, const void* ) ) ;
-
-
-void cfb_encrypt(uchar_t *input , uchar_t *output , uchar_t *iv , size_t length , size_t block_size  ,const void* key ,void (*encrypt_block)(const uchar_t* , uchar_t*, const void* ) ) ;
-void cfb_decrypt(uchar_t *input , uchar_t *output , uchar_t *iv , size_t length , size_t block_size  ,const void* key ,void (*decrypt_block)(const uchar_t* , uchar_t*, const void* ) ) ;
-
-
-void ctr_encrypt(uchar_t *input , uchar_t *output , uchar_t *iv , size_t length , size_t block_size  ,const void* key ,void (*encrypt_block)(const uchar_t* , uchar_t* , const void* ) ) ;
-void ctr_decrypt(uchar_t *input , uchar_t *output , uchar_t *iv , size_t length , size_t block_size  ,const void* key ,void (*decrypt_block)(const uchar_t* , uchar_t* , const void* ) ) ;
+// returns 0 on success otherwise non-zero for error and it woll be reported to stderr
+int ofb_encrypt(uchar_t *input , uchar_t *output , uchar_t *iv , size_t length , size_t block_size  ,const void* key ,int (*encrypt_block)(const uchar_t* , uchar_t*, const void* ) ) ;
+// returns 0 on success otherwise non-zero for error and it woll be reported to stderr
+int ofb_decrypt(uchar_t *input , uchar_t *output , uchar_t *iv , size_t length , size_t block_size  ,const void* key ,int (*decrypt_block)(const uchar_t* , uchar_t*, const void* ) ) ;
 
 
-void gcm_encrypt(uchar_t *input , uchar_t *output , uchar_t *iv , size_t length , size_t block_size  ,const void* key ,void (*encrypt_block)(const uchar_t* , uchar_t*, const void* ) ) ;
-void gcm_decrypt(uchar_t *input , uchar_t *output , uchar_t *iv , size_t length , size_t block_size  ,const void* key ,void (*decrypt_block)(const uchar_t* , uchar_t*, const void* ) ) ;
+// returns 0 on success otherwise non-zero for error and it woll be reported to stderr
+int cfb_encrypt(uchar_t *input , uchar_t *output , uchar_t *iv , size_t length , size_t block_size  ,const void* key ,int (*encrypt_block)(const uchar_t* , uchar_t*, const void* ) ) ;
+// returns 0 on success otherwise non-zero for error and it woll be reported to stderr
+int cfb_decrypt(uchar_t *input , uchar_t *output , uchar_t *iv , size_t length , size_t block_size  ,const void* key ,int (*decrypt_block)(const uchar_t* , uchar_t*, const void* ) ) ;
+
+
+// returns 0 on success otherwise non-zero for error and it woll be reported to stderr
+int ctr_encrypt(uchar_t *input , uchar_t *output , uchar_t *iv , size_t length , size_t block_size  ,const void* key ,int (*encrypt_block)(const uchar_t* , uchar_t* , const void* ) ) ;
+// returns 0 on success otherwise non-zero for error and it woll be reported to stderr
+int ctr_decrypt(uchar_t *input , uchar_t *output , uchar_t *iv , size_t length , size_t block_size  ,const void* key ,int (*decrypt_block)(const uchar_t* , uchar_t* , const void* ) ) ;
+
+
+// returns 0 on success otherwise non-zero for error and it woll be reported to stderr
+int gcm_encrypt(uchar_t *input , uchar_t *output , uchar_t *iv , size_t length , size_t block_size  ,const void* key ,int (*encrypt_block)(const uchar_t* , uchar_t*, const void* ) ) ;
+// returns 0 on success otherwise non-zero for error and it woll be reported to stderr
+int gcm_decrypt(uchar_t *input , uchar_t *output , uchar_t *iv , size_t length , size_t block_size  ,const void* key ,int (*decrypt_block)(const uchar_t* , uchar_t*, const void* ) ) ;
 
 
 
--- src/common/block_cipher_modes_operation.c
+++ src/common/block_cipher_modes_operation.c
@@ -1,6 +1,10 @@
 #include "block_cipher_modes_operation.h"
 #include "cipher_interface.h"
 /*
+    IMPORTANT NOTE : in these methods we are NOT checking the output of `encrypt_block` or `decrypt_block` .
+
+    TODO : you know the whole idea of this is really not it at all , 
+
     TODO : OFB is not tested with official test vectors
     TODO : OFB for decryption needs a better approach cuz the same function is needed for both encrypt + decrypt 
     TODO : same thing with CTR (tested it with tineyAES in test.c line 239) although it has the problem of same function called twice
@@ -80,7 +84,7 @@ void blockcipher_decrypt_modeop(uchar_t *input , uchar_t *output ,uchar_t *iv , 
     }
 }
 
-void ecb_encrypt(uchar_t *input , uchar_t *output , size_t length , size_t block_size  ,const void* key ,void (*encrypt_block)(const uchar_t* , uchar_t*  , const void* ) ) {
+int ecb_encrypt(uchar_t *input , uchar_t *output , size_t length , size_t block_size  ,const void* key ,int (*encrypt_block)(const uchar_t* , uchar_t*  , const void* ) ) {
 
     // simple dimple check
     size_t t = length / block_size ;
@@ -88,42 +92,44 @@ void ecb_encrypt(uchar_t *input , uchar_t *output , size_t length , size_t block
     if (t*block_size != length)
     {
         fprintf(stderr , "ERROR: make sure the input is padded or smth cuz length/block_size = %ld/%ld = %f \n" , length , block_size , (float) (length/block_size) ) ;
-        return;
+        return 1;
     }
+    printf("INFO: contnuing tho the size is incompatile ...\n") ; 
     
-    printf("looping from 0 to %ld\n", t);
-
+    printf("INFO : looping from 0 to %ld\n", t);
+    
     for (size_t i = 0; i < t ; i++)
     {
         encrypt_block(input + i*block_size , output + i*block_size , key) ;
     }
     
-
+    return 0 ;
 }
 
-void ecb_decrypt(uchar_t *input , uchar_t *output , size_t length , size_t block_size  ,const void* key ,void (*decrypt_block)(const uchar_t* , uchar_t* , const void* ) ) {
-
+int ecb_decrypt(uchar_t *input , uchar_t *output , size_t length , size_t block_size  ,const void* key ,int (*decrypt_block)(const uchar_t* , uchar_t* , const void* ) ) {
+    
     // simple dimple check
     size_t t = length / block_size ;
     // assert()
     if (t*block_size != length)
     {
         fprintf(stderr , "ERROR: make sure the input is padded or smth cuz length/block_size = %ld/%ld = %f \n" , length , block_size , (float) (length/block_size) ) ;
-        return;
+        return 1;
     }
+    printf("INFO: contnuing tho the size is incompatile ...\n") ; 
     
-
+    printf("INFO : looping from 0 to %ld\n", t);
 
     for (size_t i = 0; i < t ; i++)
     {
         decrypt_block(input + i*block_size , output + i*block_size , key) ;
     }
     
-
+    return 0 ;
 }
 
 
-void cbc_encrypt(uchar_t *input , uchar_t *output , uchar_t *iv , size_t length , size_t block_size  ,const void* key ,void (*encrypt_block)(const uchar_t* , uchar_t* , const void* ) ) {
+int cbc_encrypt(uchar_t *input , uchar_t *output , uchar_t *iv , size_t length , size_t block_size  ,const void* key ,int (*encrypt_block)(const uchar_t* , uchar_t* , const void* ) ) {
 
     // simple dimple check
     size_t t = length / block_size ;
@@ -131,11 +137,15 @@ void cbc_encrypt(uchar_t *input , uchar_t *output , uchar_t *iv , size_t length 
     if (t*block_size != length || length < block_size )
     {
         fprintf(stderr , "ERROR: (length < block_size ) OR make sure the input is padded or smth cuz length/block_size = %ld/%ld = %f \n" , length , block_size , (float) (length/block_size) ) ;
-        return;
+        return 1;
     }
     
     uchar_t *buffer =(uchar_t*) malloc(sizeof(uchar_t)*block_size) ; 
-    assert(buffer != NULL && "ummmm buy more ram lol");
+    if (buffer == NULL) {
+        fprintf(stderr , "ERROR: malloc couldnt allocate  \n" , sizeof(uchar_t)*block_size ) ;
+        return 2 ;
+    }
+    // assert(buffer != NULL && "ummmm buy more ram lol");
 
     mapOperation(input , iv , buffer , block_size , binaryXorUchar) ;
     
@@ -149,20 +159,24 @@ void cbc_encrypt(uchar_t *input , uchar_t *output , uchar_t *iv , size_t length 
     
 
     free(buffer);
+    return 0 ; 
 }
 
-void cbc_decrypt(uchar_t *input , uchar_t *output , uchar_t *iv , size_t length , size_t block_size  ,const void* key ,void (*decrypt_block)(const uchar_t* , uchar_t* , const void* ) ) {
+int cbc_decrypt(uchar_t *input , uchar_t *output , uchar_t *iv , size_t length , size_t block_size  ,const void* key ,int (*decrypt_block)(const uchar_t* , uchar_t* , const void* ) ) {
 
     // simple dimple check
     size_t t = length / block_size ;
     if (t*block_size != length || length < block_size )
     {
         fprintf(stderr , "ERROR: (length < block_size ) OR make sure the input is padded or smth cuz length/block_size = %ld/%ld = %f \n" , length , block_size , (float) (length/block_size) ) ;
-        return;
+        return 1;
     }
     
     uchar_t *buffer =(uchar_t*) malloc(sizeof(uchar_t)*block_size) ; 
-    assert(buffer != NULL && "ummmm buy more ram lol");
+    if (buffer == NULL) {
+        fprintf(stderr , "ERROR: malloc couldnt allocate  \n" , sizeof(uchar_t)*block_size ) ;
+        return 2 ;
+    }
 
     decrypt_block(input , buffer  , key) ;
     mapOperation(buffer , iv , output , block_size , binaryXorUchar) ;
@@ -176,22 +190,25 @@ void cbc_decrypt(uchar_t *input , uchar_t *output , uchar_t *iv , size_t length 
     
 
     free(buffer);    
+    return 0 ; 
 }
 
 
-void ofb_encrypt(uchar_t *input , uchar_t *output , uchar_t *iv , size_t length , size_t block_size  ,const void* key ,void (*encrypt_block)(const uchar_t* , uchar_t*, const void* ) ) {
+int ofb_encrypt(uchar_t *input , uchar_t *output , uchar_t *iv , size_t length , size_t block_size  ,const void* key ,int (*encrypt_block)(const uchar_t* , uchar_t*, const void* ) ) {
     size_t t = length / block_size ;
     if (t*block_size != length || length < block_size )
     {
         fprintf(stderr , "ERROR: (length < block_size ) OR make sure the input is padded or smth cuz length/block_size = %ld/%ld = %f \n" , length , block_size , (float) (length/block_size) ) ;
-        return;
+        return 1;
     }
 
     // basically the Si
     uchar_t *prev_si =(uchar_t*) malloc(sizeof(uchar_t)*block_size) ; 
     uchar_t *cur_si =(uchar_t*) malloc(sizeof(uchar_t)*block_size) ; 
-    assert(prev_si != NULL && "ummmm buy more ram lol");
-    assert(cur_si != NULL && "ummmm buy more ram lol");
+    if (prev_si == NULL || cur_si == NULL ) {
+        fprintf(stderr , "ERROR: malloc couldnt allocate  \n" , sizeof(uchar_t)*block_size ) ;
+        return 2 ;        
+    }
 
     encrypt_block(iv , cur_si  , key) ;
     mapOperation(cur_si , input , output , block_size , binaryXorUchar) ;
@@ -208,23 +225,28 @@ void ofb_encrypt(uchar_t *input , uchar_t *output , uchar_t *iv , size_t length 
     free(cur_si);
     free(prev_si);
 
+    return 0 ; 
 }
 
-
-void ofb_decrypt(uchar_t *input , uchar_t *output , uchar_t *iv , size_t length , size_t block_size  ,const void* key ,void (*decrypt_block)(const uchar_t* , uchar_t*, const void* ) ) {
+// TODO : NOT IMPLEMENTED
+int ofb_decrypt(uchar_t *input , uchar_t *output , uchar_t *iv , size_t length , size_t block_size  ,const void* key ,int (*decrypt_block)(const uchar_t* , uchar_t*, const void* ) ) {
     // ofb_encrypt(input , output , iv , length , block_size , key , ofb_encrypt) ;
+    fprintf(stderr , "ERROR: NOT IMPLEMENTED ! \n") ; 
+    return 0 ; 
 }
 
-void ctr_encrypt(uchar_t *input , uchar_t *output , uchar_t *iv , size_t length , size_t block_size  ,const void* key ,void (*encrypt_block)(const uchar_t* , uchar_t* , const void* ) ) {
+int ctr_encrypt(uchar_t *input , uchar_t *output , uchar_t *iv , size_t length , size_t block_size  ,const void* key ,int (*encrypt_block)(const uchar_t* , uchar_t* , const void* ) ) {
     size_t t = length / block_size ;
     if (t*block_size != length || length < block_size )
     {
         fprintf(stderr , "ERROR: (length < block_size ) OR make sure the input is padded or smth cuz length/block_size = %ld/%ld = %f \n" , length , block_size , (float) (length/block_size) ) ;
-        return;
+        return 1;
     }
     uchar_t *buffer =(uchar_t*) malloc(sizeof(uchar_t)*block_size) ; 
-    assert(buffer != NULL && "ummmm buy more ram lol");
-    
+    if (buffer == NULL) {
+        fprintf(stderr , "ERROR: malloc couldnt allocate  \n" , sizeof(uchar_t)*block_size ) ;
+        return 2 ;
+    }    
     encrypt_block(iv , buffer  , key) ;
     mapOperation(buffer , input , output , block_size , binaryXorUchar) ;
     
@@ -238,24 +260,31 @@ void ctr_encrypt(uchar_t *input , uchar_t *output , uchar_t *iv , size_t length 
     
     free(buffer);
     
+    return 0 ; 
 }
 
-void ctr_decrypt(uchar_t *input , uchar_t *output , uchar_t *iv , size_t length , size_t block_size  ,const void* key ,void (*decrypt_block)(const uchar_t* , uchar_t* , const void* ) ) {
+// TODO : NOT IMPLEMENTED
+int ctr_decrypt(uchar_t *input , uchar_t *output , uchar_t *iv , size_t length , size_t block_size  ,const void* key ,int (*decrypt_block)(const uchar_t* , uchar_t* , const void* ) ) {
     // ctr_encrypt(input , output , iv , length , block_size , key , decrypt_block) ;
-    
+    fprintf(stderr , "ERROR: NOT IMPLEMENTED ! \n") ; 
+
+    return 0 ;    
 }
 
-void cfb_encrypt(uchar_t *input , uchar_t *output , uchar_t *iv , size_t length , size_t block_size  ,const void* key ,void (*encrypt_block)(const uchar_t* , uchar_t*, const void* ) ) {
+int cfb_encrypt(uchar_t *input , uchar_t *output , uchar_t *iv , size_t length , size_t block_size  ,const void* key ,int (*encrypt_block)(const uchar_t* , uchar_t*, const void* ) ) {
     size_t t = length / block_size ;
     if (t*block_size != length || length < block_size )
     {
         fprintf(stderr , "ERROR: (length < block_size ) OR make sure the input is padded or smth cuz length/block_size = %ld/%ld = %f \n" , length , block_size , (float) (length/block_size) ) ;
-        return;
+        return 1 ;
     }
 
     // basically the Si
     uchar_t *buffer =(uchar_t*) malloc(sizeof(uchar_t)*block_size) ; 
-    assert(buffer != NULL && "ummmm buy more ram lol");
+    if (buffer == NULL) {
+        fprintf(stderr , "ERROR: malloc couldnt allocate  \n" , sizeof(uchar_t)*block_size ) ;
+        return 2 ;
+    }
 
     encrypt_block(iv , buffer  , key) ;
 
@@ -269,20 +298,24 @@ void cfb_encrypt(uchar_t *input , uchar_t *output , uchar_t *iv , size_t length 
     
 
     free(buffer);
-    
+
+    return 0 ;
 }
 
-void cfb_decrypt(uchar_t *input , uchar_t *output , uchar_t *iv , size_t length , size_t block_size  ,const void* key ,void (*decrypt_block)(const uchar_t* , uchar_t*, const void* ) ) {
+int cfb_decrypt(uchar_t *input , uchar_t *output , uchar_t *iv , size_t length , size_t block_size  ,const void* key ,int (*decrypt_block)(const uchar_t* , uchar_t*, const void* ) ) {
     size_t t = length / block_size ;
     if (t*block_size != length || length < block_size )
     {
         fprintf(stderr , "ERROR: (length < block_size ) OR make sure the input is padded or smth cuz length/block_size = %ld/%ld = %f \n" , length , block_size , (float) (length/block_size) ) ;
-        return;
+        return 1 ;
     }
 
     // basically the Si
     uchar_t *buffer =(uchar_t*) malloc(sizeof(uchar_t)*block_size) ; 
-    assert(buffer != NULL && "ummmm buy more ram lol");
+    if (buffer == NULL) {
+        fprintf(stderr , "ERROR: malloc couldnt allocate  \n" , sizeof(uchar_t)*block_size ) ;
+        return 2 ;
+    }
 
     decrypt_block(iv , buffer  , key) ;
 
@@ -297,5 +330,6 @@ void cfb_decrypt(uchar_t *input , uchar_t *output , uchar_t *iv , size_t length 
 
     free(buffer);
 
+    return 0; 
 }
 
